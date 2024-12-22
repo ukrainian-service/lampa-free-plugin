21627760d3fb4b732bb9043fdb33c620ca2cef7e
ukrainian-service <matviyenkovitaliy1990@gmail.com>
2024-12-22 23:11:48 +0200

    Update free_plugin.js

--- free_plugin.js
+++ free_plugin.js
@@ -8,7 +8,7 @@
                 name: 'Eneyida',
                 url: 'https://eneyida.tv/',
                 parser: function (query) {
-                    return `${this.url}search/${encodeURIComponent(query)}`;
+                    return `${this.url}?do=search&q=${encodeURIComponent(query)}`;
                 }
             },
             {
@@ -26,6 +26,61 @@
                 }
             }
         ];
+
+        Lampa.Component.add('free_plugin', function () {
+            this.create = function () {
+                this.activity.loader(true);
+                const query = this.activity.query;
+                const searchResults = [];
+
+                let completedRequests = 0;
+                sources.forEach(source => {
+                    const url = source.parser(query);
+                    Lampa.Network.silent(url, (response) => {
+                        searchResults.push({
+                            source: source.name,
+                            url,
+                            content: response
+                        });
+                        completedRequests++;
+                        if (completedRequests === sources.length) {
+                            this.showResults(searchResults);
+                        }
+                    }, () => {
+                        completedRequests++;
+                        if (completedRequests === sources.length) {
+                            this.showResults(searchResults);
+                        }
+                    });
+                });
+            };
+
+            this.showResults = function (results) {
+                this.activity.loader(false);
+                if (results.length === 0) {
+                    Lampa.Noty.show('Результати не знайдені.');
+                    return;
+                }
+
+                results.forEach(result => {
+                    const item = Lampa.Template.get('button', {
+                        title: `${result.source} - Перейти`,
+                        description: result.url
+                    });
+
+                    item.on('hover:enter', () => {
+                        Lampa.Platform.openURL(result.url);
+                    });
+
+                    this.append(item);
+                });
+            };
+        });
+    }
+
+    createFreePlugin();
+})();
+
 
         Lampa.Component.add('free_plugin', function () {
             this.create = function () {
